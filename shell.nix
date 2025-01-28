8b791b2e72e1f25b00e706292647c6f6a0db6cc9
Maarten Behn <maarten.behn@gmail.com>
2025-01-28 14:21:21 +0100

    init

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell rec {
+
+  name = "obsidian_export";
+  RUSTC_VERSION = "stable";
+
+  shellHook = ''
+    export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+    '';
+
+  packages = with pkgs; [
+    rustup
+  ];
+}
+
+
