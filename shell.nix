3f78e94c415df87300a4d0c5cc7f73c2dfd91912
Maarten Behn <maarten.behn@gmail.com>
2025-01-28 23:40:32 +0100

    remove

--- shell.nix
+++ shell.nix
@@ -3,17 +3,9 @@
 pkgs.mkShell rec {
 
   name = "obsidian_export";
-  RUSTC_VERSION = "stable";
-
-  shellHook = ''
-    export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-    '';
 
   packages = with pkgs; [
-    rustup
     nodejs_22
-    hugo
   ];
 }
 
