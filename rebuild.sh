d4500334768f1dcc3d63615b5c39e47be550e573
Maarten Behn <46872913+MaartenBehn@users.noreply.github.com>
2025-07-28 22:16:45 +0200

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,7 @@
 
 git pull
 
+rustup default stable
 export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
 export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
 
